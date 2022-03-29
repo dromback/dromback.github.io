6f0605facadccc359c9af8acc7ef6c8707d9def4
dromback <101027701+dromback@users.noreply.github.com>
2022-03-29 11:36:34 -0500

    Create theme.js

---
+++ the-meta-portal-v2/styles/theme.js
@@ -0,0 +1,10 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const color = {
+    dark: "black",
+    light: "white"
+}
+
+const customTheme = extendTheme({color})
+
+export default customTheme
